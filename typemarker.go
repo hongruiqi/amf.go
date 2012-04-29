74522f66cc593c05d479d16fa199ae1bc1a29f76
hongruiqi <hongruiqi@gmail.com>
2012-04-29 18:30:40 +0800

    typemarker

---
+++ typemarker.go
@@ -0,0 +1,21 @@
+package amf
+
+const (
+	NumberMarker = iota
+	BooleanMarker
+	StringMarker
+	ObjectMarker
+	MovieclipMarker
+	NullMarker
+	UndefinedMarker
+	ReferenceMarker
+	EcmaArrayMarker
+	ObjectEndMarker
+	StrictArrayMarker
+	DateMarker
+	LongStringMarker
+	UnsupportedMarker
+	RecordsetMarker
+	XmlDocumentMarker
+	TypedObjectMarker
+)
