0eec8f7f8aee0d17830e937a86b3f70768d9dd9e
hongruiqi <hongruiqi@gmail.com>
2012-05-01 10:57:02 +0800

    amf3

---
+++ amf3/types.go
@@ -0,0 +1,27 @@
+package amf3
+
+type UndefinedType struct {
+}
+
+type NullType struct {
+}
+
+type FalseType struct {
+}
+
+type TrueType struct {
+}
+
+type IntegerType int
+type DoubleType float64
+type StringType string
+type XMLDocumentType string
+type DateType struct {
+}
+type ArrayType struct {
+}
+type ObjectType struct {
+}
+type XMLType string
+type ByteArrayType struct {
+}
